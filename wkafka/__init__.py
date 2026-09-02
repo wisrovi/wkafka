81049969a68fdd7febd26cc4137fcf58d8fbfb95
wisrovi rodriguez <wisrovi.rodriguez@gmail.com>
2026-09-02 12:40:08 +0200

    [DOCS] Synchronize package version to 1.0.8

--- wkafka/__init__.py
+++ wkafka/__init__.py
@@ -6,7 +6,7 @@ from wkafka.controller import wkafka
 # Support legacy import: from wkafka.wkafka import Wkafka / import wkafka.wkafka
 sys.modules["wkafka.wkafka"] = wkafka
 
-__version__ = "1.0.0"
+__version__ = "1.0.8"
 __all__ = ["WKafka", "Message", "Wkafka", "wkafka"]
 
 # Alias for backward compatibility if needed, promoting WKafka (CamelCase)
