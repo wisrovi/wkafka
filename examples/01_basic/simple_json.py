db15419a65b55af11fd402a102f24a964ab1f9e2
wisrovi rodriguez <wisrovi.rodriguez@gmail.com>
2026-09-02 10:03:53 +0200

    [CHORE] Adjust rebalance sleep time in simple_json example

--- examples/01_basic/simple_json.py
+++ examples/01_basic/simple_json.py
@@ -1,15 +1,18 @@
-import time
 import threading
+import time
+
 from wkafka import WKafka
 
 kafka = WKafka(dynamic_group_id=True)
+
 
 @kafka.consumer(topic="basic_json", format="json")
 def on_message(msg):
     print(f"✅ Recibido: {msg.value}")
 
+
 threading.Thread(target=lambda: kafka.run_consumers(block=True), daemon=True).start()
-time.sleep(2)
+time.sleep(4)  # Wait for consumer group rebalance
 
 print("🚀 Enviando mensaje...")
 with kafka.producer() as p:
