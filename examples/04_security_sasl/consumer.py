27b96b1673aa6f045c6f8f1f42b4cf29977398fb
wisrovi rodriguez <wisrovi.rodriguez@gmail.com>
2026-09-02 12:09:58 +0200

    [FEATURE] update files

--- examples/04_security_sasl/consumer.py
+++ examples/04_security_sasl/consumer.py
@@ -1,13 +1,18 @@
 from wkafka import WKafka
+
 sasl_config = {
     "security_protocol": "SASL_PLAINTEXT",
     "sasl_mechanism": "PLAIN",
     "sasl_plain_username": "external-user",
-    "sasl_plain_password": "mdL0Q9gKAANuglBV8KaGvPYS6NihQP5u"
+    "sasl_plain_password": "mdL0Q9gKAANuglBV8KaGvPYS6NihQP5u",
 }
 kafka = WKafka(bootstrap_servers="localhost:30092", dynamic_group_id=True, **sasl_config)
+
+
 @kafka.consumer(topic="secure_topic", format="json")
 def on_secure_msg(msg):
     print(f"🔒 Recibido en canal seguro: {msg.value}")
+
+
 if __name__ == "__main__":
     kafka.run_consumers(block=True)
