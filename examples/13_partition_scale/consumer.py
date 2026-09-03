fdc0da19c45364809c132e1d4eb28bc56fc3c752
wisrovi rodriguez <wisrovi.rodriguez@gmail.com>
2026-09-03 09:14:41 +0200

    [STYLE] Format partition scaling consumer example code according to Black standard

--- examples/13_partition_scale/consumer.py
+++ examples/13_partition_scale/consumer.py
@@ -14,13 +14,17 @@ TOPIC_NAME = "partition_scaled_topic"
 # Worker 1
 @kafka.consumer(topic=TOPIC_NAME, format="json", key_filter="key_1")
 def worker_1(msg):
-    print(f"👷 [WORKER 1] Processed job {msg.value.get('job_id')} on partition offset {msg.offset}")
+    print(
+        f"👷 [WORKER 1] Processed job {msg.value.get('job_id')} on partition offset {msg.offset}"
+    )
 
 
 # Worker 2
 @kafka.consumer(topic=TOPIC_NAME, format="json")
 def worker_2(msg):
-    print(f"👷 [WORKER 2] Processed job {msg.value.get('job_id')} on partition offset {msg.offset}")
+    print(
+        f"👷 [WORKER 2] Processed job {msg.value.get('job_id')} on partition offset {msg.offset}"
+    )
 
 
 if __name__ == "__main__":
