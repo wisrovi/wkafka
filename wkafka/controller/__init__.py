c22aea940ffa613ec2b1f91d8b10375c631373a4
wisrovi rodriguez <wisrovi.rodriguez@gmail.com>
2026-09-03 11:16:02 +0200

    [STYLE] Format imports and add module docstring in controller __init__.py

--- wkafka/controller/__init__.py
+++ wkafka/controller/__init__.py
@@ -1,5 +1,6 @@
+"""Controller package module providing simplified WKafka interface aliases."""
+
 from .wkafka import Wkafka
 from wkafka.core.models import Message as Consumer_data
 
 __all__ = ["Wkafka", "Consumer_data"]
-
