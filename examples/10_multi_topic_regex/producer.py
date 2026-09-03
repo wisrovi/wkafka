be9053a0071165dd1e2db39d4e270db298e9bd3e
wisrovi rodriguez <wisrovi.rodriguez@gmail.com>
2026-09-03 09:14:41 +0200

    [STYLE] Format multi-topic producer example code according to Black standard

--- examples/10_multi_topic_regex/producer.py
+++ examples/10_multi_topic_regex/producer.py
@@ -11,11 +11,17 @@ kafka = WKafka(bootstrap_servers="localhost:9092")
 def main():
     print("producir a varios tópicos (sensor_temp, sensor_humidity)...")
     with kafka.producer() as p:
-        p.send("sensor_temp", value={"metric": "temperature", "value": 24.5}, format="json")
+        p.send(
+            "sensor_temp", value={"metric": "temperature", "value": 24.5}, format="json"
+        )
         print("Sent to sensor_temp")
         time.sleep(1)
 
-        p.send("sensor_humidity", value={"metric": "humidity", "value": 60.0}, format="json")
+        p.send(
+            "sensor_humidity",
+            value={"metric": "humidity", "value": 60.0},
+            format="json",
+        )
         print("Sent to sensor_humidity")
         time.sleep(1)
 
